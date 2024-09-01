e715838f11e985139f34e43b840f5e1a2859b5ba
Jairus Keith <jairuskeith20@gmail.com>
2024-09-01 10:11:31 -0600

    first commit

---
+++ src/main/java/com/beeble/ecommerce/entity/ProductCategory.java
@@ -0,0 +1,24 @@
+package com.beeble.ecommerce.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Set;
+
+@Getter
+@Setter
+@Entity
+@Table(name="product_category")
+public class ProductCategory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @Column(name = "categoryName")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    private Set<Product> product;
+}
